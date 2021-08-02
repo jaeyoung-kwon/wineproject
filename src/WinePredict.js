325747e8c21919c1c49f199b8890e4f2ada559b8
jaeyoung-kwon <61729032+jaeyoung-kwon@users.noreply.github.com>
2021-08-02 23:38:12 +0900

    main

--- src/WinePredict.js
+++ src/WinePredict.js
@@ -49,98 +49,127 @@ const WinePredict = () => {
     }
   }
   return (
-    <article className="bg-red-900 h-screen">
-      <form className="flex flex-col text-white" action="/predict" method="post"
+    <article className="h-screen">
+      <form className="flex flex-col" action="/predict" method="post"
         onSubmit={function(e){
           e.preventDefault();
           console.log(checkedInputs)
         }.bind(this)}
       >
         <header >
-          <h1 className="flex items-center justify-center font-medium">와인 추천</h1>
+          <h1 className="flex items-center justify-center font-medium py-10">와인 추천</h1>
           <h2 className="flex items-center justify-center font-medium">세가지의 설문을 통해서 고객님에 맞는 와인을 추천해 드립니다.</h2>
         </header>
-          <h3 className="flex items-center justify-center font-medium py-5">와인을 드시는 시간은 언제인가요?</h3>
+          <h3 className="flex items-center justify-center font-medium py-5">1. 와인을 드시는 시간은 언제인가요?</h3>
         <firstarticle className="flex items-center justify-center">
-          <input
-            name="checktime"
-            type="checkbox"
-            id="1"
-            onClick={()=>{changeCheck('1')}}
-          ></input>
-          <h4>아침</h4>
-          <input
-            name="checktime"
-            type="checkbox"
-            id="2"
-            onClick={()=>{changeCheck('2')}}
-          ></input>
-          <h4>점심</h4>
-          <input
-            name="checktime"
-            type="checkbox"
-            id="3"
-            onClick={()=>{changeCheck('3')}}
-          ></input>
-          <h4>저녁</h4>
+          <anr className="px-6 flex flex-row items-center">
+            <input
+              name="checktime"
+              type="checkbox"
+              id="1"
+              onClick={()=>{changeCheck('1')}}
+            ></input>
+            <h4 className="px-2">아침</h4>
+          </anr>
+          <anr className="px-6 flex flex-row items-center">
+            <input
+              name="checktime"
+              type="checkbox"
+              id="2"
+              onClick={()=>{changeCheck('2')}}
+            ></input>
+            <h4 className="px-2">점심</h4>
+          </anr>
+          <anr className="px-6 flex flex-row items-center">
+            <input
+              name="checktime"
+              type="checkbox"
+              id="3"
+              onClick={()=>{changeCheck('3')}}
+            ></input>
+            <h4 className="px-2">저녁</h4>
+          </anr>
         </firstarticle>
-          <h3 className="flex items-center justify-center font-medium py-5">누구와 함께 드시나요?</h3>
+          <h3 className="flex items-center justify-center font-medium py-5">2. 누구와 함께 드시나요?</h3>
         <secondarticle className="flex items-center justify-center">
-          <input
-            name="checkperson"
-            type="checkbox"
-            id="4"
-            onClick={()=>{changeCheck('4')}}
-          ></input>
-          <h4>나홀로</h4>
-          <input
-            name="checkperson"
-            type="checkbox"
-            id="5"
-            onClick={()=>{changeCheck('5')}}
-          ></input>
-          <h4>연인과</h4>
-          <input
-            name="checkperson"
-            type="checkbox"
-            id="6"
-            onClick={()=>{changeCheck('6')}}
-          ></input>
-          <h4>친구와</h4>
-          <input
-            name="checkperson"
-            type="checkbox"
-            id="7"
-            onClick={()=>{changeCheck('7')}}
-          ></input>
-          <h4>가족과</h4>
+          <anr className="px-6 flex flex-row items-center">
+            <input
+              name="checkperson"
+              type="checkbox"
+              id="4"
+              onClick={()=>{changeCheck('4')}}
+            ></input>
+            <h4 className="px-2">나홀로</h4>
+          </anr>
+          <anr className="px-6 flex flex-row items-center">
+            <input
+              name="checkperson"
+              type="checkbox"
+              id="5"
+              onClick={()=>{changeCheck('5')}}
+            ></input>
+            <h4 className="px-2">연인과</h4>
+          </anr>
+          <anr className="px-6 flex flex-row items-center">
+            <input
+              name="checkperson"
+              type="checkbox"
+              id="6"
+              onClick={()=>{changeCheck('6')}}
+            ></input>
+            <h4 className="px-2">친구와</h4>
+          </anr>
+          <anr className="px-6 flex flex-row items-center">
+            <input
+              name="checkperson"
+              type="checkbox"
+              id="7"
+              onClick={()=>{changeCheck('7')}}
+            ></input>
+            <h4 className="px-2">가족과</h4>
+          </anr>
         </secondarticle>
-          <h3 className="flex items-center justify-center font-medium py-5">와인을 어떨때 드시나요?</h3>
+          <h3 className="flex items-center justify-center font-medium py-5">3. 와인을 어떨때 드시나요?</h3>
         <thirdarticle className="flex items-center justify-center">
-          <input
-            name="checkwhen"
-            type="checkbox"
-            id="8"
-            onClick={()=>{changeCheck('8')}}
-          ></input>
-          <h4>행복할 때</h4>
-          <input
-            name="checkwhen"
-            type="checkbox"
-            id="9"
-            onClick={()=>{changeCheck('9')}}
-          ></input>
-          <h4>점심</h4>
-          <input
-            name="checkwhen"
-            type="checkbox"
-            id="10"
-            onClick={()=>{changeCheck('10')}}
-          ></input>
-          <h4>저녁</h4>
+          <anr className="px-6 flex flex-row items-center">
+            <input
+              name="checkwhen"
+              type="checkbox"
+              id="8"
+              onClick={()=>{changeCheck('8')}}
+            ></input>
+            <h4 className="px-2">행복할 때</h4>
+          </anr>
+          <anr className="px-6 flex flex-row items-center">
+            <input
+              name="checkwhen"
+              type="checkbox"
+              id="9"
+              onClick={()=>{changeCheck('9')}}
+            ></input>
+            <h4 className="px-2">신날 때</h4>
+          </anr>
+          <anr className="px-6 flex flex-row items-center">
+            <input
+              name="checkwhen"
+              type="checkbox"
+              id="10"
+              onClick={()=>{changeCheck('10')}}
+            ></input>
+            <h4 className="px-2">외로울 때</h4>
+          </anr>
+          <anr className="px-6 flex flex-row items-center">
+            <input
+              name="checkwhen"
+              type="checkbox"
+              id="11"
+              onClick={()=>{changeCheck('11')}}
+            ></input>
+            <h4 className="px-2">화날 때</h4>
+          </anr>
         </thirdarticle>
         <lastarticle className="flex items-center justify-center py-4">
-          <input type="submit" className="bg-black"></input>
+          <input type="submit" className="ml-8 whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-black rounded-xl shadow-sm text-base font-medium text-black hover:bg-black hover:text-white"></input>
         </lastarticle>
       </form>
     </article>
